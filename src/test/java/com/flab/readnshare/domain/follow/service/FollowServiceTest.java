dc67429d91b92327d87492af80f943b207b40f60
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2025-03-17 18:50:43 +0900

    test: 특정 유저 팔로워 조회
    JwtAuthInterceptor를 피하기 위해 WebMvcConfig exclude
    JPA 메타 모델 만들기 위해 @MockBean(JpaMetamodelMappingContext.class) 추가
    
    Resolves: #28

--- src/test/java/com/flab/readnshare/domain/follow/service/FollowServiceTest.java
+++ src/test/java/com/flab/readnshare/domain/follow/service/FollowServiceTest.java
@@ -5,6 +5,7 @@ import com.flab.readnshare.domain.follow.domain.Follow;
 import com.flab.readnshare.domain.follow.repository.FollowRepository;
 import com.flab.readnshare.domain.member.domain.Member;
 import com.flab.readnshare.global.common.exception.FollowException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,9 +13,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -38,6 +42,31 @@ class FollowServiceTest {
         // when & then
         assertThrows(FollowException.DuplicateFollowException.class, () ->
                 followService.save(fromMember, toMember));
+    }
+
+    @DisplayName("특정 사용자의 팔로우 목록을 조회한다.")
+    @Test
+    void getFollowers() throws Exception {
+        // Given
+        Member toMember = FollowTestFixture.getMemberEntity();
+        Member fromMember1 = FollowTestFixture.getMemberEntity();
+        Member fromMember2 = FollowTestFixture.getMemberEntity();
+        Follow follow1 = FollowTestFixture.getFollowEntity(fromMember1, toMember);
+        Follow follow2 = FollowTestFixture.getFollowEntity(fromMember2, toMember);
+
+        given(followRepository.findByToMember(eq(toMember)))
+                .willReturn(List.of(follow1, follow2));
+
+        // When
+        List<Follow> followers = followService.getFollowers(toMember);
+
+        // Then
+        Assertions.assertThat(followers).hasSize(2)
+                .extracting("fromMember")
+                .containsExactlyInAnyOrder(
+                        fromMember1,
+                        fromMember2
+                );
     }
 
 }
