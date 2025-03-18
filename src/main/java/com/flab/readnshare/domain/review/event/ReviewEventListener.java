5d79176e209ddb2bdf54782b05bf1306fa726819
psg <1999byh@naver.com>
2025-03-18 12:54:06 +0900

    feat: 리뷰 삭제 시 피드에도 업데이트 반영하기
    - ReviewEvent를 Create, Delete로 세분화
    - ReviewEventListener에 Delete EventListener 추가
    
    Resolves: #50

--- src/main/java/com/flab/readnshare/domain/review/event/ReviewEventListener.java
+++ src/main/java/com/flab/readnshare/domain/review/event/ReviewEventListener.java
@@ -17,7 +17,7 @@ public class ReviewEventListener {
     private final FeedFacade feedFacade;
 
     @TransactionalEventListener
-    private void updateFollowersFeed(ReviewEvent event) {
+    private void updateFollowersFeed(ReviewCreateEvent event) {
         Member writer = event.getMember();
         Review review = event.getReview();
 
@@ -25,6 +25,17 @@ public class ReviewEventListener {
 
         if (!followerIds.isEmpty()) {
             feedFacade.addToFeed(followerIds, review);
+        }
+    }
+
+    @TransactionalEventListener
+    private void deleteReviewFromFeed(ReviewDeleteEvent event) {
+        Member writer = event.getMember();
+        Long reviewId = event.getReviewId();
+
+        List<Long> followerIds = followService.getFollowerIds(writer);
+        if (!followerIds.isEmpty()) {
+            feedFacade.deleteToFeed(followerIds, reviewId);
         }
     }
 }
