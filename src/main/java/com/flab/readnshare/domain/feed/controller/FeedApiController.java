441e9a4a8a6ba3301fa9221efbe8550b1e73b656
psg <1999byh@naver.com>
2025-03-20 14:15:37 +0900

    refactor: FeedApiController 리팩토링
    - RequestBody를 ModelAttribute로 변경
    - Swagger 적용
    
    Resolves: #112

--- src/main/java/com/flab/readnshare/domain/feed/controller/FeedApiController.java
+++ src/main/java/com/flab/readnshare/domain/feed/controller/FeedApiController.java
@@ -5,11 +5,18 @@ import com.flab.readnshare.domain.feed.dto.FeedResponseDto;
 import com.flab.readnshare.domain.feed.facade.FeedFacade;
 import com.flab.readnshare.domain.member.domain.Member;
 import com.flab.readnshare.global.common.resolver.SignInMember;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +25,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/feeds")
 @RequiredArgsConstructor
+@Tag(name = "Feed", description = "피드")
 public class FeedApiController {
     private final FeedFacade feedFacade;
 
     @GetMapping
-    public ResponseEntity<List<FeedResponseDto>> getFeeds(@SignInMember Member member, @RequestBody FeedRequestDto dto) {
+    @Operation(summary = "피드 조회", description = "자신의 피드를 조회할 때 사용하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다."),
+            @ApiResponse(responseCode = "401", description = "토큰이 없습니다.", content = @Content(mediaType = "application/json",
+                    examples = {
+                            @ExampleObject(name = "JWT_NULL", value = "{ \"code\": \"JWT_NULL\", \"message\": \"토큰이 없습니다.\" }")
+                    }))
+    })
+    public ResponseEntity<List<FeedResponseDto>> getFeeds(@ModelAttribute FeedRequestDto dto, @Parameter(hidden = true) @SignInMember Member member) {
         return new ResponseEntity<>(feedFacade.getFeeds(member.getId(), dto.getLastReviewId(), dto.getLimit()), HttpStatus.OK);
     }
 }
