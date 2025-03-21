c609aa08e8fe950fec0abe1999dd81240dc0f6bd
psg <1999byh@naver.com>
2025-03-21 10:51:27 +0900

    feat: FeedApiController 입력 값 검증
    - lastReviewId 값은 1 이상이어야 함
    - limit 값은 1 이상이어야 함
    - limit 값은 10 이하이어야 함
    
    Resolves: #127

--- src/main/java/com/flab/readnshare/domain/feed/dto/FeedRequestDto.java
+++ src/main/java/com/flab/readnshare/domain/feed/dto/FeedRequestDto.java
@@ -1,11 +1,26 @@
 package com.flab.readnshare.domain.feed.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
+@AllArgsConstructor
 public class FeedRequestDto {
+
+    @Min(value = 1, message = "lastReviewId 값은 1 이상이어야 합니다.")
     private Long lastReviewId;
-    private Integer limit;
+
+    @Min(value = 1, message = "limit 값은 1 이상이어야 합니다.")
+    @Max(value = 10, message = "limit 값은 10 이하이어야 합니다.")
+    private int limit;
+
+    public FeedRequestDto() {
+        limit = 10;
+    }
 }
