56367502987e20e14bf60f165b8a47b1448e72e8
UncleSamsun <alswns5620@naver.com>
2025-03-24 18:39:52 +0900

    refactor: 이메일 비밀번호 환경변수 설정
    - 이메일 비밀번호 환경변수 설정
    
    Resolves: #136

--- src/main/java/com/flab/readnshare/global/config/EmailConfig.java
+++ src/main/java/com/flab/readnshare/global/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.flab.readnshare.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,13 +10,17 @@ import java.util.Properties;
 
 @Configuration
 public class EmailConfig {
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername("noreply.readnshare@gmail.com");
-        mailSender.setPassword("ellrjhdhannosjfe");
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
