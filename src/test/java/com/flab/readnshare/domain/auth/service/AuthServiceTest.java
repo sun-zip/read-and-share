a904a05e88a58df55fae8008eff4436c6acca829
Isonade2 <c1004sos@naver.com>
2025-03-26 15:48:50 +0900

    refactor: auth 도메인 테스트 커버리지 보완

--- src/test/java/com/flab/readnshare/domain/auth/service/AuthServiceTest.java
+++ src/test/java/com/flab/readnshare/domain/auth/service/AuthServiceTest.java
@@ -1,6 +1,7 @@
 package com.flab.readnshare.domain.auth.service;
 
 import com.flab.readnshare.AuthTestFixture;
+import com.flab.readnshare.domain.auth.domain.RefreshToken;
 import com.flab.readnshare.domain.auth.dto.SignInRequestDto;
 import com.flab.readnshare.domain.auth.repository.RefreshTokenRepository;
 import com.flab.readnshare.domain.member.domain.Member;
@@ -18,6 +19,7 @@ import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -42,68 +44,112 @@ class AuthServiceTest {
 
     @Spy
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    //private PasswordEncoder passwordEncoder;
 
-    @Test
-    @DisplayName("로그인을 하면 member를 리턴한다")
-    void signIn_success_return_member() {
-        // given
-        SignInRequestDto request = AuthTestFixture.getSignInRequestDto();
+    @Nested
+    @DisplayName("signIn 테스트")
+    class SignInTests {
+        @Test
+        @DisplayName("성공 - member를 리턴")
+        void signIn_success_return_member() {
+            // given
+            SignInRequestDto request = AuthTestFixture.getSignInRequestDto();
 
-        Member expectedMember = Member.builder()
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .build();
+            Member expectedMember = Member.builder()
+                    .email(request.getEmail())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .build();
 
-        expectedMember.setVerified(true);
+            expectedMember.setVerified(true);
 
-        when(memberRepository.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(expectedMember));
+            when(memberRepository.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(expectedMember));
+            //when(passwordEncoder.matches(any(String.class), any(String.class))).thenReturn(true);
+            // when
+            Member foundMember = authService.signIn(request);
 
-        // when
-        Member foundMember = authService.signIn(request);
+            // then
+            assertNotNull(foundMember);
+            assertEquals(request.getEmail(), foundMember.getEmail());
+        }
 
-        // then
-        assertNotNull(foundMember);
-        assertEquals(request.getEmail(), foundMember.getEmail());
-    }
+        @Test
+        @DisplayName("실패 - 인증되지 않은 사용자")
+        void signIn_fail_unverified_email_exception() {
+            // given
+            SignInRequestDto request = AuthTestFixture.getSignInRequestDto();
 
-    @Test
-    @DisplayName("비밀번호 불일치 시 InvalidEmailOrPasswordException 발생한다")
-    void signIn_fail_password() {
-        // given
-        SignInRequestDto request = AuthTestFixture.getSignInRequestDto();
+            Member expectedMember = Member.builder()
+                    .email(request.getEmail())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .build();
 
-        Member expectedMember = Member.builder()
-                .email(request.getEmail())
-                .password("test12345!")
-                .build();
+            expectedMember.setVerified(false);
 
-        when(memberRepository.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(expectedMember));
+            when(memberRepository.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(expectedMember));
 
-        // when & then
-        assertThrows(AuthException.InvalidEmailOrPasswordException.class, () -> authService.signIn(request));
-    }
+            // when
+            assertThrows(AuthException.UnverifiedEmailException.class, () -> authService.signIn(request));
+        }
 
+        @Test
+        @DisplayName("실패 - 비밀번호 불일치")
+        void signIn_fail_invalid_email_or_password_exception() {
+            // given
+            SignInRequestDto request = AuthTestFixture.getSignInRequestDto();
 
+            Member expectedMember = Member.builder()
+                    .email(request.getEmail())
+                    .password("test12345!")
+                    .build();
 
-    @Test
-    @DisplayName("Refresh token이 Redis에 없는 경우 ExpiredTokenException이 발생한다")
-    void refresh_fail_redis() {
-        // given
-        String refreshToken = "refreshTokenValue";
-        when(refreshTokenRepository.findById(refreshToken)).thenReturn(Optional.empty());
+            when(memberRepository.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(expectedMember));
 
-        // when & then
-        assertThrows(AuthException.ExpiredTokenException.class, () -> authService.validateTokenFromRedis(refreshToken));
-
+            // when & then
+            assertThrows(AuthException.InvalidEmailOrPasswordException.class, () -> authService.signIn(request));
+        }
     }
 
     @Nested
-    @DisplayName("Refresh Token을 업데이트하면 새로운 Access Token과 Refresh Token이 발급된다.")
-    class updateRefreshTokenTests {
+    @DisplayName("validateTokenFromRedis 테스트")
+    class ValidateTokenFromRedisTests {
+        @Test
+        @DisplayName("성공 - Refresh token이 Redis에 있음")
+        void success() {
+            String refreshTokenStr = "refreshTokenValue";
+            RefreshToken refreshToken = RefreshToken.builder()
+                    .refreshTokenValue(refreshTokenStr)
+                    .memberId(1L)
+                    .expiration(1000L)
+                    .build();
+
+            when(refreshTokenRepository.findById(refreshTokenStr)).thenReturn(Optional.of(refreshToken));
+
+            authService.validateTokenFromRedis(refreshTokenStr);
+
+            verify(refreshTokenRepository).findById(refreshTokenStr);
+
+        }
 
 
         @Test
-        @DisplayName("새로운 Access Token과 Refresh Token이 발급된다.")
+        @DisplayName("실패 - Refresh token이 Redis에 없음")
+        void refresh_fail_expired_token_exception() {
+            // given
+            String refreshToken = "refreshTokenValue";
+            when(refreshTokenRepository.findById(refreshToken)).thenReturn(Optional.empty());
+
+            // when & then
+            assertThrows(AuthException.ExpiredTokenException.class, () -> authService.validateTokenFromRedis(refreshToken));
+
+        }
+    }
+
+
+    @Nested
+    @DisplayName("updateRefreshToken 테스트")
+    class updateRefreshTokenTests {
+        @Test
+        @DisplayName("성공 - 새로운 Access Token과 Refresh Token이 발급된다.")
         void updateRefreshToken_success() {
             // given
             String oldRefreshToken = "oldTokenValue";
